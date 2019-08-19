4a382b638b6051932ca9fefce93f3054eef6b023
Adrimi <adriano100417@gmail.com>
2019-08-19 11:35:36 +0200

    processing core image is working, only for sepia effect

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -6,15 +6,43 @@
 //  Copyright © 2019 Adrimi. All rights reserved.
 //
 
+import CoreImage
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "InstaFilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
 
     @IBAction func changeFilter(_ sender: Any) {
@@ -24,9 +52,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func intensityChange(_ sender: Any) {
-        #if DEBUG
-        print("intensity has changed")
-        #endif
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        // pass slider value as an intensity parameter!
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        
+        // change Core Image -> CGImage -> UIImage
+        if let cgImage = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgImage)
+            imageView.image = processedImage
+        }
     }
 }
 
