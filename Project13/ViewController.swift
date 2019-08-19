2d15dfc6b7e2d577f498cdccd25bde7f9a357c05
Adrimi <adriano100417@gmail.com>
2019-08-19 11:09:06 +0200

    UI set up

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChange(_ sender: Any) {
+        #if DEBUG
+        print("intensity has changed")
+        #endif
+    }
 }
 
